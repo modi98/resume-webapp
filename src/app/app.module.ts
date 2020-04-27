1207ccb504ae5b68abea93efc74239989a6d9d56
modi98 <mauricio.alvaradoh98@gmail.com>
2020-04-27 11:54:37 -0500

    Added bio, profile, and main menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BioComponent } from './components/bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    ProfileComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
