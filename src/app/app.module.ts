f1e703e97177c4e6c82e4c203962c7ebb229c9c3
modi98 <mauricio.alvaradoh98@gmail.com>
2020-04-27 13:16:27 -0500

    Added routes and added styles and transitions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BioComponent } from './components/bio/bio.component';
+
+const routes: Routes = [
+  { path: '', component: BioComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { BioComponent } from './components/bio/bio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-bio',
   templateUrl: './bio.component.html',
-  styleUrls: ['./bio.component.less']
+  styleUrls: ['./bio.component.less'],
+  animations: [
+    trigger('profileTransitions', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('.5s')
+      ])
+    ]),
+    trigger('bioInfoTransitions', [
+      state('in', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('.5s .5s')
+      ])
+    ])
+  ]
 })
 export class BioComponent implements OnInit {
 
