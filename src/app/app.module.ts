2eac68ec23d66eb2106643876f798797f40f7ca8
modi98 <mauricio.alvaradoh98@gmail.com>
2020-04-27 16:08:51 -0500

    Added footer with social media also added fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BioComponent } from './components/bio/bio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'bio', pathMatch: 'full' },
@@ -19,12 +20,13 @@ const routes: Routes = [
     AppComponent,
     MainMenuComponent,
     ProfileComponent,
-    BioComponent
+    BioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'}),
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.less']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sendToSocialMedia(socialMedia): void {
+    let url = '';
+    switch (socialMedia) {
+      case 'linkedin':
+        url = 'https://www.linkedin.com/in/mauricio-alvarado-6089a3169/';
+        break;
+      case 'facebook':
+        url = 'https://www.facebook.com/Mauryy98';
+        break;
+      case 'instagram':
+        url = 'https://www.instagram.com/modii_98/';
+        break;
+      case 'twitter':
+        url = 'https://twitter.com/maualvaradoh';
+        break;
+      default:
+        url = '';
+        break;
+    }
+    window.open(url);
+  }
+}
